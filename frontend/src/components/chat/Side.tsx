0c77952eeee35739205c2de4297d35c2afe3e119
Cerise Massé <cerisemasse@MacBook-Air-de-Cerise.local>
2023-01-11 13:08:34 +0100

    requet

--- frontend/src/components/chat/Side.tsx
+++ frontend/src/components/chat/Side.tsx
@@ -1,24 +1,25 @@
 //
 import '../../pages/chat/Chat.scss'
-import { RootState } from '../../state';
+// import { RootState } from '../../state';
 
 //
 import React, { useState, useEffect } from 'react';
-import { Button, IconButton } from '@mui/material';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import Box from '@mui/material/Box';
+// import { Button, IconButton } from '@mui/material';
+// import ButtonGroup from '@mui/material/ButtonGroup';
+// import Box from '@mui/material/Box';
 import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
-import { deepPurple, grey, red } from '@mui/material/colors';
-import { ListItem, List } from '@mui/material';
-import TextField from '@mui/material/TextField';
+// import Stack from '@mui/material/Stack';
+// import { deepPurple,  red } from '@mui/material/colors';
+import {grey}from '@mui/material/colors';
+// import { ListItem, List } from '@mui/material';
+// import TextField from '@mui/material/TextField';
 import AddIcon from '@mui/icons-material/Add';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
-import VolumeMuteIcon from '@mui/icons-material/VolumeMute';
-import BlockIcon from '@mui/icons-material/Block';
-import Chip from '@mui/material/Chip';
-import { useSelector } from 'react-redux';
+// import MoreVertIcon from '@mui/icons-material/MoreVert';
+// import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+// import VolumeMuteIcon from '@mui/icons-material/VolumeMute';
+// import BlockIcon from '@mui/icons-material/Block';
+// import Chip from '@mui/material/Chip';
+// import { useSelector } from 'react-redux';
 
 const Side = (props: { openConvName: string, setOpenConvName: Function, allConv: Array<{ receiver: string, last_message_text: string }> }) => {
     const [inputValue, setInputValue] = useState("");
